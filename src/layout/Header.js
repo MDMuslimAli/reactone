20b7109671ef11cc485b8e574c5e192f439fab8c
mohammed <mohammedmuslimali@gmail.com>
2023-09-16 23:41:06 +0530

    Header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,8 +1,41 @@
+import "./Layout.css";
+
 function Header() {
   return (
     <div className="Header">
       <header className="App-header">
-       Header
+        <div className="info-header">
+          <div className="container">
+            <div className="right-section">
+              <a href=""><i class="bi bi-telephone-forward-fill"></i> App Download</a>
+              <a href=""><i class="bi bi-question-circle"></i> Help</a>
+            </div>
+          </div>
+        </div>
+        <div className="menu-header">
+          <div className="container">
+            <div className="logo">NR GEMS</div>
+            <div className="menu">
+              <div className="l1-menu">
+                <nav>
+                  <a className="nav-item">one menu</a>
+                  <a className="nav-item">one menu</a>
+                  <a className="nav-item">one menu</a>
+                  <a className="nav-item">one menu</a>
+                </nav>
+              </div>
+              <div className="search-section">
+              <i class="bi bi-search"></i>
+                <input type="search" className="search-input" name="" id="" placeholder="Search"/>
+              </div>
+            </div>
+            <div className="action-button">
+              <button><i class="bi bi-person"></i> Account</button>
+              <button><i class="bi bi-heart-fill"></i> Wishlist</button>
+              <button><i class="bi bi-bag"></i> Cart</button>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
