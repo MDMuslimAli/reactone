72a5d8e661ba9cfffd768114a4701cdf4172ddd1
mohammed <mohammedmuslimali@gmail.com>
2023-09-19 16:19:42 +0530

    Routes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './layout/Header';
-import Footer from './layout/Footer';
-import reportWebVitals from './reportWebVitals';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
+import Footer from './layout/Footer';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Header />
-    <Footer />
-  </React.StrictMode>
+  <div>
+  <App />
+  <Footer />
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,12 +1,104 @@
 function Footer() {
-    return (
-      <div className="Footer">
-        <footer className="App-footer">
-         Footer
-        </footer>
-      </div>
-    );
-  }
-  
-  export default Footer;
-  
+  return (
+    <div className="Footer">
+      <footer className="App-footer">
+        <div className="priamry-footer"></div>
+        <div className="site-maps">
+          <div className="container">
+            <div className="site-map-column">
+              <p className="heading-text">Heading Text</p>
+              <div className="list-section">
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+              </div>
+            </div>
+            <div className="site-map-column">
+              <p className="heading-text">Heading Text</p>
+              <div className="list-section">
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+              </div>
+            </div>
+            <div className="site-map-column">
+              <p className="heading-text">Heading Text</p>
+              <div className="list-section">
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+              </div>
+            </div>
+            <div className="site-map-column">
+              <p className="heading-text">Heading Text</p>
+              <div className="list-section">
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+              </div>
+            </div>
+            <div className="site-map-column">
+              <p className="heading-text">Heading Text</p>
+              <div className="list-section">
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+              </div>
+            </div>
+            <div className="site-map-column">
+              <p className="heading-text">Heading Text</p>
+              <div className="list-section">
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+                <a href="#">Lorem ipsum, </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="declaration">
+          <div className="container">
+            <p>© 2023 - 2025 NR Gems. Ltd. All Rights Reserved</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
